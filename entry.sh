c8cf38044b095a9fe5c28c14e209e000a7031d71
Jay K <jay94ks@gmail.com>
2022-07-26 20:14:23 +0900

    컨테이너 실행시, 초기화 이후 수행해야 하는 스크립트를 지정하는 인자 추가.
    작업 디렉토리: /var/www/html,
    PHP 파일이어야만 실행됩니다.

--- entry.sh
+++ entry.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 
+TMP = "$@" | tr -d ' '
+HERE = `pwd`
+
 /usr/bin/php -q /root/run-env.php
+
+if [ "$TMP" != "" ];
+then
+	cd /var/www/html
+	/usr/bin/php -q $TMP
+fi
+
+cd $HERE
 /usr/sbin/apache2ctl -D FOREGROUND
