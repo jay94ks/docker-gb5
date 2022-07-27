21d30fa97b32c9119bc933d91bc3260d22f1ed2b
Jay K <jay94ks@gmail.com>
2022-07-27 17:02:05 +0900

    버그픽스: entry.sh 파일 내 LF가 에디터 상에서 CRLF로 강제 변환되어 제대로 동작하지 않는 현상 수정.

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
-docker build -t jay94ks/docker-gb5:v20220727-auto .
-docker push jay94ks/docker-gb5:v20220727-auto
+docker build -t jay94ks/docker-gb5:v20220727_3-auto .
+docker push jay94ks/docker-gb5:v20220727_3-auto
 
-docker tag jay94ks/docker-gb5:v20220727-auto jay94ks/docker-gb5:latest-auto
+docker tag jay94ks/docker-gb5:v20220727_3-auto jay94ks/docker-gb5:latest-auto
 docker push jay94ks/docker-gb5:latest-auto
 
